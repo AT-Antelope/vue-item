5de39cc83369a7164bb25a1f9d857629349dcc09
AT <q263629656@gmail.com>
2021-10-18 10:50:15 +0800

    feature-vue-item-V1.0.0-20211018

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="login">
-    <h1>this is login page</h1>
+    this is login page
     <el-row>
       <el-button>默认按钮</el-button>
       <el-button type="primary">主要按钮</el-button>
