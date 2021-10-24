4efdbb7722b90ac85a1b4531b56c0f56dee9591a
AT <q263629656@gmail.com>
2021-10-24 10:33:51 +0800

    新引入axios包，新增utils/request中的拦截器

---
+++ src/utils/request.js
@@ -0,0 +1,48 @@
+/**
+ * views/login.vue --> api/login.js --> utils/request.js
+ */
+import axios from "axios";
+
+const service = axios.create();
+
+// 添加请求拦截器
+service.interceptors.request.use(
+  function (config) {
+    // 在发送请求之前做些什么
+    return config;
+  },
+  function (error) {
+    // 对请求错误做些什么
+    return Promise.reject(error);
+  }
+);
+
+// 添加响应拦截器
+service.interceptors.response.use(
+  function (response) {
+    // 2xx 范围内的状态码都会触发该函数。
+    // 对响应数据做点什么
+    return response;
+  },
+  function (error) {
+    // 超出 2xx 范围的状态码都会触发该函数。
+    // 对响应错误做点什么
+    return Promise.reject(error);
+  }
+);
+
+//  测试接口时可以这样直接调用
+// service.request({
+//   method: "get",
+//   url: "/user/123",
+//   data: {
+//     firstName: "Fred",
+//     lastName: "Flintstone",
+//   },
+// });
+
+export default service;
+/**
+ * 使用export default时，不能同时存在多个default
+ * 在其他文件mport引入时，不需要花括号
+ */
