2b0919695580c6c8ea86ce7ed0b66c518cebc745
AT <q263629656@gmail.com>
2021-11-15 23:18:36 +0800

    (api/news)新增分类添加、获取分类接口，(信息管理/信息分类)修改为显示从服务器获取的数据，(utils/request)新增请求拦截器内的headers发送配置

--- src/api/news.js
+++ src/api/news.js
@@ -1,3 +1,4 @@
+import service from "@/utils/request";
 /**
  * 列表
  * list
@@ -12,3 +13,31 @@
  * 编辑
  * edit
  */
+
+/**
+ * 删除
+ */
+
+/**
+ * 分类添加
+ */
+export function AddFirstCategory(data) {
+  return service.request({
+    method: "post",
+    url: "/news/addFirstCategory/",
+    data,
+    // data: data，(key:value)，key和value同名的情况下，写成单一个即可
+  });
+}
+
+/**
+ * 获取分类
+ */
+export function GetCategoryAll(data) {
+  return service.request({
+    method: "post",
+    url: "/news/getCategoryAll/",
+    data,
+    // data: data，(key:value)，key和value同名的情况下，写成单一个即可
+  });
+}
